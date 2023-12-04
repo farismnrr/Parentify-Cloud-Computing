17088e30417b0c1c73ed83a6d885f0981d905580
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2023-12-04 16:51:13 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE public_jwtdb;
-USE public_jwtdb;
+CREATE DATABASE parentify;
+USE parentify;
 
 CREATE TABLE users (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	username VARCHAR(255) NOT NULL,
-	email TEXT NOT NULL,
-	password VARCHAR(255) NOT NULL,
-	created TIMESTAMP NOT NULL DEFAULT NOW()
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    email TEXT NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT NOW()
 );
