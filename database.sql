37f6029bef658b677b155ab600e764ac246b4455
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2023-12-05 07:57:32 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -7,5 +7,5 @@ CREATE TABLE users (
     email TEXT NOT NULL,
     password VARCHAR(255) NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT NOW(),
-    token TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL;
+    token TEXT CHARACTER NULL;
 );
