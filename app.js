56fe19f5cd3577a12c379d09cec2ab58c9433d79
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2023-12-10 18:07:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const port = 8080;
 
 app.use('/', foodRoutes);
 
+// Middleware untuk menangani kesalahan 404 (Not Found)
+app.use((req, res, next) => {
+    res.status(404).send('Halaman tidak ditemukan');
+});
+
+// Middleware untuk menangani kesalahan umum (500 Internal Server Error)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Terjadi kesalahan pada server');
+});
+
 app.listen(port, () => {
     console.log(`Server berjalan di port ${port}`);
 });
