6bf3dd52d566b7b6f16f7dc36fda626e38981c77
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2023-12-13 17:24:57 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE parentify;
+CREATE DATABASE IF NOT EXISTS parentify;
 USE parentify;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     email TEXT NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE users (
     token TEXT NULL
 );
 
-CREATE TABLE foods (
+CREATE TABLE IF NOT EXISTS foods (
     id INT PRIMARY KEY AUTO_INCREMENT,
     img VARCHAR(255),
     name VARCHAR(255) NOT NULL,
@@ -19,12 +19,10 @@ CREATE TABLE foods (
     nutrition VARCHAR(255)
 );
 
-CREATE TABLE classification (
+CREATE TABLE IF NOT EXISTS classification (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     information TEXT,
     status VARCHAR(255),
     texture VARCHAR(255)
 );
-
-exit;
