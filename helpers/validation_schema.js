1b6d27ab1f30dab4a0372426f1c83738c5ffb688
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2023-12-20 21:06:03 +0700

    Update validation_schema.js

--- helpers/validation_schema.js
+++ helpers/validation_schema.js
@@ -19,6 +19,7 @@ const registrationSchema = Joi.object({
     username: Joi.string().alphanum().min(3).max(30).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
+    confirmPassword: Joi.string().min(6).required(),
 });
 
 const loginSchema = Joi.object({
